5eb57e9701b33c56ecb6182e3052065963dad4f0
amreyesbriones@gmail.com <CETECOM@AOAWSB02LC0831.duoc.acad>
2024-04-15 20:37:32 -0400

    subo archivo main

---
+++ main.py
@@ -0,0 +1,38 @@
+# cliclo for --> para
+
+#for x in range(3):
+    #print(f"Menu {x}")
+    
+ 
+# contadorNota = 0
+# try:
+#     cantidad = int(input("ingrese cantidad de notas a promediar\n"))
+#     for x in range(cantidad):
+#             nota = float(input("ingrese note {x+1}"))
+#             contadorNota = contadorNota + nota 
+#     promedio = round(contadorNota/cantidad)   
+#     print(f"el resultado de las {cantidad} notas es: {promedio}")    
+# except:
+#     print("tipo de dato no compatible")      
+
+
+####################################################################################
+
+bandera = True
+
+while bandera:
+    numero = int(input("ingrese un numero"))
+    if numero%2==0:
+        print("puede elegir otro numero")
+    else:
+        print("el numero es impar, se acabo el ciclo")
+        bandera = False
+        
+print("gracias por ocupar esta wea")
+
+
+a = 10 
+while a >= 10:
+    numero = int(input("ingrese numero\n"))
+print("chao")
+
